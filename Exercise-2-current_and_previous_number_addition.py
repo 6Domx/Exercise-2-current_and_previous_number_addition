e8d6c82e8a8ac05b54b9483317a9883aa2f4a1c1
unknown <miggy.payumo26@gmail.com>
2024-01-25 01:58:52 +0800

    Code can now accept given values

--- Exercise-2-current_and_previous_number_addition.py
+++ Exercise-2-current_and_previous_number_addition.py
@@ -2,3 +2,9 @@
 # 1. Ask for a number input from the user
 # 2. Analyze the number and the next 10 consecutive number starting from the given value
 # 3. Print the 10 consecutive number starting from the given value
+
+initial_value = input("Please input a value:")
+if initial_value.isdigit():
+    initial_value = int(initial_value)
+else:
+    print("Invalid, Value must be in numbers: ")
