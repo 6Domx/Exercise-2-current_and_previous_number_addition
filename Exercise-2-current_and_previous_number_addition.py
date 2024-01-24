69012cab2d21a388dca5ee96d6ea9b600e632e33
unknown <miggy.payumo26@gmail.com>
2024-01-25 02:12:27 +0800

    code can now print 10 consecutive numbers but is not giving the right previous numbers

--- Exercise-2-current_and_previous_number_addition.py
+++ Exercise-2-current_and_previous_number_addition.py
@@ -3,8 +3,17 @@
 # 2. Analyze the number and the next 10 consecutive number starting from the given value
 # 3. Print the 10 consecutive number starting from the given value
 
-initial_value = input("Please input a value:")
-if initial_value.isdigit():
-    initial_value = int(initial_value)
+given_value = input("Please input a value:")
+if given_value.isdigit():
+    given_value = int(given_value)
 else:
     print("Invalid, Value must be in numbers: ")
+
+highest_value = given_value + 10
+
+for i in range(1, highest_value):
+    sum = given_value + i
+    print("Current number is =", i, " Previous Number is =", given_value, " Sum =", sum,)
+
+given_value = i
+    
