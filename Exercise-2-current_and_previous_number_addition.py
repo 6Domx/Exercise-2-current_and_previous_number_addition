07f00632d01dd08739383deb6b8bdf5ee64f7296
unknown <miggy.payumo26@gmail.com>
2024-01-25 02:26:02 +0800

    limit for highest value has been set

--- Exercise-2-current_and_previous_number_addition.py
+++ Exercise-2-current_and_previous_number_addition.py
@@ -10,10 +10,10 @@ else:
     print("Invalid, Value must be in numbers: ")
 
 highest_value = given_value + 10
-
-for i in range(1, highest_value):
+starting_value = given_value - 1
+for i in range(given_value, highest_value):
     sum = given_value + i
-    print("Current number is =", i, " Previous Number is =", given_value, " Sum =", sum,)
+    print("Current number is =", i, " Previous Number is =", starting_value, " Sum =", sum,)
 
-given_value = i
+    given_value = i
     
