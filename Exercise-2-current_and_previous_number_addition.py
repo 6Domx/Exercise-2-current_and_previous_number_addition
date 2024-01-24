a693dc692cfc245a900b8943e25ae0d38909eeb7
unknown <miggy.payumo26@gmail.com>
2024-01-25 02:39:26 +0800

    Code Labeled

--- Exercise-2-current_and_previous_number_addition.py
+++ Exercise-2-current_and_previous_number_addition.py
@@ -3,17 +3,21 @@
 # 2. Analyze the number and the next 10 consecutive number starting from the given value
 # 3. Print the 10 consecutive number starting from the given value
 
-given_value = input("Please input a value:")
-if given_value.isdigit():
-    given_value = int(given_value)
-else:
-    print("Invalid, Value must be in numbers: ")
 
-highest_value = given_value + 11
+# Asking for a number from the user and making sure it only accepts integers
+while True:
+    given_value = input("Please input a value:")
+    if given_value.isdigit():
+        given_value = int(given_value)
+    else:
+        print("Invalid, Value must be in numbers: ")
+        break
+    highest_value = given_value + 11
 
-for i in range(given_value, highest_value):
-    sum = given_value + i
-    print("GIVEN | PREVIOUS | SUM")
-    print(i, "   +   ", given_value, "   =", sum)
+    for i in range(given_value, highest_value):
+        sum = given_value + i
+        print("GIVEN | PREVIOUS | SUM")
+        print(i, "   +   ", given_value, "   =", sum)
 
-    given_value = i
+        given_value = i
+        
