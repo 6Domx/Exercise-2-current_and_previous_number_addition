d5c17ff6fa90aebb18dc4acef12c0935d0a876ce
unknown <miggy.payumo26@gmail.com>
2024-01-25 01:54:45 +0800

    Pseudocode

---
+++ Exercise-2-current_and_previous_number_addition.py
@@ -0,0 +1,4 @@
+# Pseudocode
+# 1. Ask for a number input from the user
+# 2. Analyze the number and the next 10 consecutive number starting from the given value
+# 3. Print the 10 consecutive number starting from the given value
